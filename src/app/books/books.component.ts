70cb6ffa164739f30c9cefbf987db6e640808efa
shindebhavika <bhavikashinde74@gmail.com>
2024-02-09 11:03:50 +0530

    two-way-bunding-using-ngmodal

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -2,21 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-books',
-  // standalone: true,
-  // imports: [],
+  
   templateUrl: './books.component.html',
-  styleUrls:['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent {
-name="bhavika";
-src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg";
-name2="radhika";
-src2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5PkW4fJsvhTn3s9hnv2nSU7a5jkGYsUH9Zl7YOHZKeA&s";
-isDisabled=false;
+  name = 'bhavikaaaa';
+  src =
+    'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg';
+  name2 = 'radhika';
+  src2 =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5PkW4fJsvhTn3s9hnv2nSU7a5jkGYsUH9Zl7YOHZKeA&s';
+  isDisabled = false;
 
-handleClick(){
-  this.isDisabled=true
-console.log("arra")
-document.body.style.backgroundColor = 'lightblue';
-}
+  handleClick() {
+    this.isDisabled = true;
+  
+    document.body.style.backgroundColor = 'lightblue';
+  }
+
+  myName = '';
+  //after ngmodal function js not needed
+  // handleInput(event: any) {
+  //   console.log(event.target.value);
+  //   this.myName = event.target.value;
+  // }
 }
