15df092ee07ccb307afdf92953614fe3f85c71e0
shindebhavika <bhavikashinde74@gmail.com>
2024-02-09 09:50:02 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { BooksComponent } from "./books/books.component";
+
+@NgModule({
+  declarations: [AppComponent, BooksComponent],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  // standalone: true,
+  // imports: [],
+  templateUrl: './books.component.html',
+  styleUrls:['./books.component.css']
+})
+export class BooksComponent {
+name="bhavika";
+src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg";
+name2="radhika";
+src2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5PkW4fJsvhTn3s9hnv2nSU7a5jkGYsUH9Zl7YOHZKeA&s"
+}
