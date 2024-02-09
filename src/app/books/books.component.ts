83712b638d7d622474578307fb9200f2cfc9efa9
shindebhavika <bhavikashinde74@gmail.com>
2024-02-09 10:28:25 +0530

    event binding

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -11,5 +11,12 @@ export class BooksComponent {
 name="bhavika";
 src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg";
 name2="radhika";
-src2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5PkW4fJsvhTn3s9hnv2nSU7a5jkGYsUH9Zl7YOHZKeA&s"
+src2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5PkW4fJsvhTn3s9hnv2nSU7a5jkGYsUH9Zl7YOHZKeA&s";
+isDisabled=false;
+
+handleClick(){
+  this.isDisabled=true
+console.log("arra")
+document.body.style.backgroundColor = 'lightblue';
+}
 }
